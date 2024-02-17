0559dafd669c81d5790fc16e49f1658d317de895
anshRS <anshraj0426@gmail.com>
2024-02-17 21:08:31 +0530

    Routed links in Navbar

--- apps/frontend/src/components/Navbar.tsx
+++ apps/frontend/src/components/Navbar.tsx
@@ -4,26 +4,29 @@ import * as React from "react"
 import { Button } from "@/components/ui/button"
 import { AngleIcon, GitHubLogoIcon } from "@radix-ui/react-icons"
 import { ModeToggle } from "./Toggle"
+import Link from "next/link"
 
 const Navbar = () => {
-    return (
-        <div className="flex items-center justify-between mx-8 my-5 gap-3">
-			<div className="flex items-center gap-3 text-2xl">
+	return (
+		<div className="flex items-center justify-between mx-8 py-3 gap-3">
+			<Link href="/" className="flex items-center gap-3 text-2xl">
 				<AngleIcon className="h-8 w-8" />
 				<h3 className="font-bold">chatdroid</h3>
-			</div>
+			</Link>
 			<div className="flex gap-2">
-				<Button variant="outline" size="icon">
-					<GitHubLogoIcon className="h-4 w-4" />
+				<Button variant="outline" size="icon" asChild>
+					<Link href="https://github.com/anshRS/chatbot" target="_blank" rel="noopener noreferrer">
+						<GitHubLogoIcon className="h-4 w-4" />
+					</Link>
 				</Button>
 				<ModeToggle />
-				<Button>
-					Login
+				<Button asChild>
+					<Link href="/auth/login">Login</Link>
 				</Button>
 			</div>
 
 		</div>
-    )
+	)
 }
 
 export default Navbar
