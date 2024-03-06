7aeb5153f3b857946b30ad38b9368887c3ea1191
anshRS <anshraj0426@gmail.com>
2024-03-06 23:11:06 +0530

    Integrated login API with error handling

--- apps/frontend/src/components/auth/LoginForm.tsx
+++ apps/frontend/src/components/auth/LoginForm.tsx
@@ -1,22 +1,31 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import * as Yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { useRouter } from 'next/navigation';
+import axios from '@/utils/axios';
 
 import { Button } from "@/components/ui/button"
 import {
     Form,
     FormControl,
-    FormDescription,
     FormField,
     FormItem,
     FormLabel,
     FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+
+import {
+    Alert,
+    AlertDescription,
+    AlertTitle,
+} from "@/components/ui/alert"
+import { ExclamationTriangleIcon, UpdateIcon } from '@radix-ui/react-icons';
+import { useDispatch } from 'react-redux';
+import { logIn } from '@/redux/slices/auth';
+import Link from 'next/link';
 
 interface LoginFormInterface {
     email: string;
@@ -29,6 +38,10 @@ const LoginSchema = Yup.object().shape({
 });
 
 const LoginForm = () => {
+    const dispatch = useDispatch();
+
+    const [server_error, setServerError] = useState<string | null>(null);
+    const [loading, setLoading] = useState(false);
 
     const defaultValues = {
         email: "",
@@ -40,47 +53,81 @@ const LoginForm = () => {
         defaultValues,
     });
 
-    const router = useRouter();
-
-    const onSubmit = (data: LoginFormInterface) => {
+    const onSubmit = async (data: LoginFormInterface) => {
         // make api call here
-        console.log(data)
-        router.push('/chat/')
+        try {
+            setLoading(true);
+            const response = await axios.post("/auth/jwt/create/", {
+                ...data,
+            }, {
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            })
+            setServerError(null);
+            dispatch(logIn({
+                isLoggedIn: true,
+                token: response.data.access,
+            }))
+        } catch (error: any) {
+            setServerError(error.detail)
+        } finally {
+            setLoading(false);
+        }
     }
 
     return (
-        <Form {...methods}>
-            <form onSubmit={methods.handleSubmit(onSubmit)} className="space-y-2">
-                <FormField
-                    control={methods.control}
-                    name="email"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Email</FormLabel>
-                            <FormControl>
-                                <Input placeholder='Enter your email' {...field} />
-                            </FormControl>                            
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={methods.control}
-                    name="password"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Password</FormLabel>
-                            <FormControl>
-                                <Input placeholder='Enter your password' {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />                
+        <>
+            <Form {...methods}>
+                <form onSubmit={methods.handleSubmit(onSubmit)} className="space-y-2">
+                    <FormField
+                        control={methods.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input placeholder='Enter your email' autoComplete='email' {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={methods.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input placeholder='Enter your password' type='password' autoComplete='off' {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
 
-                <Button type="submit" className='w-full'>Login</Button>
-            </form>
-        </Form>
+                    <Link href="/account/reset-password" className='text-blue-500 hover:text-blue-800'>
+                        Forgot password?
+                    </Link>
+
+                    <Button type="submit" className='w-full' disabled={loading}>
+                        {loading && <UpdateIcon className='animate-spin h-4 w-4 mr-2' />}
+                        Login
+                    </Button>
+                </form>
+            </Form>
+
+            {server_error && (
+                <Alert variant="destructive" className='mt-5'>
+                    <ExclamationTriangleIcon className="h-4 w-4" />
+                    <AlertTitle>Error</AlertTitle>
+                    <AlertDescription>
+                        {server_error}
+                    </AlertDescription>
+                </Alert>
+            )}
+        </>
     )
 }
 
