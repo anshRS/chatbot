8e1214dae8ab6bcb0d31a98351a1130196309283
anshRS <anshraj0426@gmail.com>
2024-03-06 19:47:49 +0530

    Updated custom superuser command for custom Django User model

--- apps/backend/custom_commands/management/commands/superuser.py
+++ apps/backend/custom_commands/management/commands/superuser.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 
 from decouple import config
 
-DJANGO_SUPERUSER_USERNAME = config('DJANGO_SUPERUSER_USERNAME', cast=str)
+DJANGO_SUPERUSER_NAME = config('DJANGO_SUPERUSER_NAME', cast=str)
 DJANGO_SUPERUSER_PASSWORD = config('DJANGO_SUPERUSER_PASSWORD', cast=str)
 DJANGO_SUPERUSER_EMAIL = config('DJANGO_SUPERUSER_EMAIL', cast=str)
 
@@ -13,14 +13,13 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         try:
             User = get_user_model()
-            if not User.objects.filter(username=DJANGO_SUPERUSER_USERNAME).exists():
+            if not User.objects.filter(full_name=DJANGO_SUPERUSER_NAME).exists():
                 user = User(
                     email=DJANGO_SUPERUSER_EMAIL,
-                    username=DJANGO_SUPERUSER_USERNAME,
+                    full_name=DJANGO_SUPERUSER_NAME,
                 )
                 user.set_password(DJANGO_SUPERUSER_PASSWORD)
-                user.is_superuser = True
-                user.is_staff = True
+                user.is_superuser = True                
                 user.is_admin = True
                 user.save()
                 self.stdout.write(self.style.SUCCESS('-----------Successfully created new superuser-----------'))
